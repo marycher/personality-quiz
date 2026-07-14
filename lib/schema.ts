2da9f851809f66a34281dc2358f334f5fc31f938
Mariia Cherkashina <maria-cherk@yandex-team.ru>
2026-07-14 20:27:30 +0300

    Fix schema types

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,3 +1,5 @@
+import { ScalarAttributeType } from "@aws-sdk/client-dynamodb";
+
 export const TableName = {
   QUESTIONS: "questions",
   QUIZ_RESULTS: "quiz_results",
@@ -7,13 +9,13 @@ export const TABLE_SCHEMAS = [
   {
     TableName: TableName.QUESTIONS,
     KeySchema: [{ AttributeName: "id", KeyType: "HASH" }],
-    AttributeDefinitions: [{ AttributeName: "id", AttributeType: "S" }],
+    AttributeDefinitions: [{ AttributeName: "id", AttributeType: ScalarAttributeType.S }],
     BillingMode: "PAY_PER_REQUEST",
   },
   {
     TableName: TableName.QUIZ_RESULTS,
     KeySchema: [{ AttributeName: "id", KeyType: "HASH" }],
-    AttributeDefinitions: [{ AttributeName: "id", AttributeType: "S" }],
+    AttributeDefinitions: [{ AttributeName: "id", AttributeType: ScalarAttributeType.S }],
     BillingMode: "PAY_PER_REQUEST",
   },
 ];
