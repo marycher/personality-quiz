6a9e61bb57522ec49eb13925bf2f8cb4befca7e9
Mariia Cherkashina <maria-cherk@yandex-team.ru>
2026-07-14 19:41:48 +0300

    Fix layout.tsx - remove fonts and handshake

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import HandshakeListener from "@/components/handshake-listener";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const appName = "Я знаю Алину лучше всех";
 
@@ -29,12 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
         <Toaster />
-        <HandshakeListener />
       </body>
     </html>
   );
